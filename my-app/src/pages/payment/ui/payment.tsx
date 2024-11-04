2ba182b710c56a4f0244f63078bf6f1219b7a17d
sergey <st@ttsgroup.su>
2024-11-04 12:56:06 +0300

    fix style

--- my-app/src/pages/payment/ui/payment.tsx
+++ my-app/src/pages/payment/ui/payment.tsx
@@ -71,126 +71,129 @@ const Form = () => {
 		navigate('/payment-success', { state: requestData });
 	};
 	return (
-		<div className={styles.form}>
-			<h2>Иван К. собирает 10 000 ₽ на "Экскурсия"</h2>
-			<FormProvider {...methods}>
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<Row>
-						<InputNumberMaskComponent
-							main={{
-								label: { text: 'Номер карты', required: true },
-								placeholder: '0000 0000 0000 0000',
-							}}
-							rules={{
-								required: { message: 'Поле обязательное', value: true },
-							}}
-							name={'card'}
-							control={control}
-						/>
-					</Row>
-					<Row gutter={[8, 8]}>
-						<Col lg={12} span={24}>
+		<div className={styles.container}>
+			<div className={styles.form}>
+				<h2>Иван К. собирает 10 000 ₽ на "Экскурсия"</h2>
+				<FormProvider {...methods}>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<Row>
 							<InputNumberMaskComponent
 								main={{
-									label: { text: 'Срок действия', required: true },
-									placeholder: 'ММ/ГГ',
-									mask: '99/99',
+									label: { text: 'Номер карты', required: true },
+									placeholder: '0000 0000 0000 0000',
 								}}
 								rules={{
 									required: { message: 'Поле обязательное', value: true },
 								}}
-								name={'expiry'}
+								name={'card'}
 								control={control}
 							/>
-						</Col>
-						<Col lg={12} span={24}>
-							<InputPassword
+						</Row>
+						<Row gutter={[8, 8]}>
+							<Col lg={12} span={24}>
+								<InputNumberMaskComponent
+									main={{
+										label: { text: 'Срок действия', required: true },
+										placeholder: 'ММ/ГГ',
+										mask: '99/99',
+									}}
+									rules={{
+										required: { message: 'Поле обязательное', value: true },
+									}}
+									name={'expiry'}
+									control={control}
+								/>
+							</Col>
+							<Col lg={12} span={24}>
+								<InputPassword
+									main={{
+										label: { text: 'CVV', required: true },
+										placeholder: 'Введите CVV',
+									}}
+									rules={{
+										required: { message: 'Поле обязательное', value: true },
+										maxLength: { value: 3, message: 'Должно содержать максимум 3 символа' },
+										onChange: (e: any) => {
+											const value = e.target.value.replace(/\D/g, '');
+											e.target.value = value;
+											setValue('cvv', value);
+										},
+									}}
+									name={'cvv'}
+									control={control}
+								/>
+							</Col>
+						</Row>
+						<Row>
+							<InputNumberComponent
 								main={{
-									label: { text: 'CVV', required: true },
-									placeholder: 'Введите CVV',
+									label: { text: 'Сумма перевода', required: true },
+									placeholder: 'Введите сумму',
+									prefix: '₽',
 								}}
 								rules={{
 									required: { message: 'Поле обязательное', value: true },
-									maxLength: { value: 3, message: 'Должно содержать максимум 3 символа' },
-									onChange: (e: any) => {
-										const value = e.target.value.replace(/\D/g, '');
-										e.target.value = value;
-										setValue('cvv', value);
-									},
+									min: { value: 10, message: 'Сумма должна быть не менее 10 рублей' },
 								}}
-								name={'cvv'}
+								name={'amount'}
 								control={control}
 							/>
-						</Col>
-					</Row>
-					<Row>
-						<InputNumberComponent
-							main={{
-								label: { text: 'Сумма перевода', required: true },
-								placeholder: 'Введите сумму',
-								prefix: '₽',
-							}}
-							rules={{
-								required: { message: 'Поле обязательное', value: true },
-								min: { value: 10, message: 'Сумма должна быть не менее 10 рублей' },
-							}}
-							name={'amount'}
-							control={control}
-						/>
-					</Row>
-					<Row>
-						<InputText
-							name={'name'}
-							main={{
-								label: { text: 'Ваше имя', required: true },
-								placeholder: 'Введите имя',
-							}}
-							control={control}
-							rules={{
-								required: { message: 'Поле обязательное', value: true },
-								validate: {
-									// Ограничение длины в байтах
-									maxBytes: (value) => {
-										const byteLength = new TextEncoder().encode(value).length;
-										return byteLength <= 50 || 'Длина не должна превышать 50 байт';
+						</Row>
+						<Row>
+							<InputText
+								name={'name'}
+								main={{
+									label: { text: 'Ваше имя', required: true },
+									placeholder: 'Введите имя',
+								}}
+								control={control}
+								rules={{
+									required: { message: 'Поле обязательное', value: true },
+									validate: {
+										// Ограничение длины в байтах
+										maxBytes: (value) => {
+											const byteLength = new TextEncoder().encode(value).length;
+											return byteLength <= 50 || 'Длина не должна превышать 50 байт';
+										},
 									},
-								},
-							}}
-						/>
-					</Row>
-					<Row>
-						<InputText
-							name={'message'}
-							main={{
-								label: 'Сообщение получателю',
-								placeholder: '',
-								allowClear: true,
-								maxLength: 50,
-								showCount: true,
-							}}
-							control={control}
-							rules={{
-								validate: {
-									maxBytes: (value) => {
-										const byteLength = new TextEncoder().encode(value).length;
-										return byteLength <= 50 || 'Длина не должна превышать 50 байт';
+								}}
+							/>
+						</Row>
+						<Row>
+							<InputText
+								name={'message'}
+								main={{
+									label: 'Сообщение получателю',
+									placeholder: '',
+									allowClear: true,
+									maxLength: 50,
+									showCount: true,
+								}}
+								control={control}
+								rules={{
+									validate: {
+										maxBytes: (value) => {
+											const byteLength = new TextEncoder().encode(value).length;
+											return byteLength <= 50 || 'Длина не должна превышать 50 байт';
+										},
 									},
-								},
-							}}
-						/>
-					</Row>
-					<div className={styles.btn}>
-						<Button type={'primary'} htmlType={'submit'}>
-							Перевести
-						</Button>
-						<Button type={'default'} onClick={() => navigate('/')}>
-							Вернуться
-						</Button>
-					</div>
-				</form>
-			</FormProvider>
+								}}
+							/>
+						</Row>
+						<div className={styles.btn}>
+							<Button type={'primary'} htmlType={'submit'}>
+								Перевести
+							</Button>
+							<Button type={'default'} onClick={() => navigate('/')}>
+								Вернуться
+							</Button>
+						</div>
+					</form>
+				</FormProvider>
+			</div>
 		</div>
 	);
+
 	function validateCardNumberLuhn(cardNumber: string) {
 		let sum = 0;
 		let shouldDouble = false;
